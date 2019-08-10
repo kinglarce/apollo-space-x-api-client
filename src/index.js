172b1c1a26ca0647ddad6e96c27bfd2cd0b9b668
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-10 18:39:07 +0900

    Add querying to server

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/',
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
